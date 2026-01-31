250b65d7e59a0afe41b9c293c3a064b0598629ce
VaquitaIsHere <saldanawitman7@gmail.com>
2026-01-30 19:05:20 -0500

    HOLA JAMON 2

--- app/src/main/java/unc/edu/pe/appdiazsaldana/EditReservationActivity.java
+++ app/src/main/java/unc/edu/pe/appdiazsaldana/EditReservationActivity.java
@@ -10,6 +10,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
 import java.util.Calendar;
 
@@ -44,10 +46,10 @@ public class EditReservationActivity extends AppCompatActivity {
             loadReservationData();
         }
 
+
         String[] areas = {"Salón Principal", "Terraza", "Salón VIP"};
         ArrayAdapter<String> areaAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, areas);
         editDiningArea.setAdapter(areaAdapter);
-
 
         editDiningArea.setOnItemClickListener((parent, view, position, id) -> {
             String selectedArea = (String) parent.getItemAtPosition(position);
@@ -56,7 +58,6 @@ public class EditReservationActivity extends AppCompatActivity {
 
         editReservationDate.setOnClickListener(v -> showDatePickerDialog());
 
-
         saveButton.setOnClickListener(v -> saveChanges());
     }
 
@@ -64,7 +65,7 @@ public class EditReservationActivity extends AppCompatActivity {
         editClientName.setText(currentReservation.getClientName());
         editTableNumber.setText(String.valueOf(currentReservation.getTableNumber()));
         editNumberOfPeople.setText(String.valueOf(currentReservation.getNumberOfPeople()));
-        editDiningArea.setText(currentReservation.getDiningArea(), false); // false para no filtrar
+        editDiningArea.setText(currentReservation.getDiningArea(), false);
         editReservationDate.setText(currentReservation.getReservationDate());
         textCourtesyDrink.setText("Bebida: " + currentReservation.getCourtesyDrink());
         imageArea.setImageResource(Reservation.getImageResourceForArea(currentReservation.getDiningArea()));
@@ -78,7 +79,6 @@ public class EditReservationActivity extends AppCompatActivity {
 
         DatePickerDialog datePickerDialog = new DatePickerDialog(this,
                 (view, year1, monthOfYear, dayOfMonth) -> {
-                    // Formateamos la fecha y la ponemos en el TextView
                     String selectedDate = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year1;
                     editReservationDate.setText(selectedDate);
                 }, year, month, day);
@@ -86,23 +86,32 @@ public class EditReservationActivity extends AppCompatActivity {
     }
 
     private void saveChanges() {
-
         if (editClientName.getText().toString().isEmpty() || editTableNumber.getText().toString().isEmpty()) {
             Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
             return;
         }
 
+        new MaterialAlertDialogBuilder(this)
+                .setTitle("Confirmar Cambios")
+                .setMessage("¿Estás seguro de que deseas guardar las modificaciones?")
+                .setNegativeButton("Cancelar", null)
+                .setPositiveButton("Guardar", (dialog, which) -> {
 
+                    proceedToSave();
+                })
+                .show();
+    }
+
+    private void proceedToSave() {
         currentReservation.setClientName(editClientName.getText().toString());
         currentReservation.setTableNumber(Integer.parseInt(editTableNumber.getText().toString()));
         currentReservation.setDiningArea(editDiningArea.getText().toString());
         currentReservation.setReservationDate(editReservationDate.getText().toString());
         currentReservation.setNumberOfPeople(Integer.parseInt(editNumberOfPeople.getText().toString()));
 
-
         Intent resultIntent = new Intent();
         resultIntent.putExtra("UPDATED_RESERVATION", currentReservation);
         setResult(RESULT_OK, resultIntent);
-        finish();
+        finish(); // Cerramos la pantalla de edición
     }
 }
